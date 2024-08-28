35f36006fa278d216709fb379d9a540ad522ad83
Pulkit Chopra <pulkitchopra690@gmail.com>
2024-08-28 21:22:39 +0530

    route changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
                 element:<Login/>
             },
             {
-                path:"onebox",
+                path:"",
                 element:<OneBox/>
             },
             {
